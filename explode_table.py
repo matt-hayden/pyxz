21e106ef655b1c2675eefb0a31b86b4fbdc2aa6d
Matt Hayden <hayden+github@morp.holo.gy>
2012-10-23 12:58:35 -0600

    minor bugs

--- explode_table.py
+++ explode_table.py
@@ -7,13 +7,18 @@ try:
 	parse_number_formatter = Decimal
 except ImportError:
 	parse_number_formatter = float
-
-def parse_number(s, formatter=parse_number_formatter):
-	if '%' in s:
-		return parse_number(s.replace('%', '', 1))/formatter(100.0)
-	return formatter(s)
-
-class TableExploder:
+#
+class FileArgument: # abstract
+	def load_file(self, filename):
+		self.filename = filename
+		self.fileobj = open(filename)
+	def __init__(self, f):
+		try:
+			if exists(f):
+				self.load_file(f)
+		except:
+			self.fileobj = f
+class TableExploder(FileArgument): # abstract
 	def get_rows(self):
 		with self.fileobj as fi:
 			cr = csv.reader(fi, delimiter=self.delimiter)
@@ -28,29 +33,22 @@ class TableExploder:
 			for sidelabel, val in elements:
 				yield (toplabel+sidelabel, self.parser(val))
 	__iter__ = get_elements
-	def load_file(self, filename):
-		self.fileobj = open(filename)
-	def __init__(self, f):
-		try:
-			if exists(f):
-				self.load_file(f)
-		except:
-			self.fileobj = f
+
 class explode_table(TableExploder):
 	delimiter='\t'
 	formatter=parse_number_formatter
 	def parser(self, s):
 		if '%' in s:
-			return parse_number(s.replace('%', '', 1))/self.formatter(100.0)
+			return self.formatter(s.replace('%', '', 1))/self.formatter(100.0)
 		return self.formatter(s)
 	def print_table(self, show_corner = False, sort_key=lambda x: x[1], sort_reverse=True):
 		elements = [ (n, l) for n, l in self.get_elements() ]
 		if sort_key:
 			elements.sort(key=sort_key, reverse=sort_reverse)
 		if show_corner:
-			print t.cornerlabel
+			print self.cornerlabel
 		for n, l in elements:
 			print n,l
 		
 if __name__ == '__main__':
-	explode_table('my-frequencies.tab').print_table()
+	explode_table('my-frequencies.tab').print_table(show_corner = True)
