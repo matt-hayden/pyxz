8dd1648449efee52ecfa9b9ba883225117c77884
Matt Hayden <hayden+github@morp.holo.gy>
2012-11-03 19:57:39 -0600

    Console color scheme changed

--- console_colors.py
+++ console_colors.py
@@ -1,37 +1,95 @@
-import argparse, sys
-from itertools import count, izip
+import sys
+
 try:
 	from _winreg import *
 except ImportError:
 	print >>sys.stderr, "Windows-only _winreg module required"
 #
 class WindowsConsoleColor:
+	colors = 16
 	permissions = KEY_ALL_ACCESS
 	#permissions = KEY_READ
 	value_type = REG_DWORD
-	typical_defaults = [ 0x000000, 0x800000, 0x008000, 0x808000, 0x000080, 0x800080, 0x008080, 0xc0c0c0, 0x808080, 0xff0000, 0x00ff00, 0xffff00, 0x0000ff, 0xff00ff, 0x00ffff, 0xffffff ]
+	typical_defaults = [ 0x000000, 
+						 0x800000, 
+						 0x008000, 
+						 0x808000, 
+						 0x000080, 
+						 0x800080, 
+						 0x008080, 
+						 0xc0c0c0, 
+						 0x808080, 
+						 0xff0000, 
+						 0x00ff00, 
+						 0xffff00, 
+						 0x0000ff, 
+						 0xff00ff, 
+						 0x00ffff, 
+						 0xffffff ]
 	#
-	def __init__(self):
-		self.key = OpenKey(HKEY_CURRENT_USER, "Console", 0, self.permissions)
+	@staticmethod
+	def GetSettingsByApplication():
+		regkey = OpenKey(HKEY_CURRENT_USER, "Console", 0, KEY_READ)
+		index = 0
+		for i in range(1024):
+			try:
+				yield EnumKey(regkey,index)
+				index += 1
+			except WindowsError:
+				raise StopIteration
+	#
+	def __init__(self, KeyName="Console", Computer=None):
+		self.regkey = OpenKey(HKEY_CURRENT_USER, KeyName, 0, self.permissions)
+		if Computer:
+			self.regkey = ConnectRegistry(Computer, self.regkey)
 	#def __del__(self):
-	#	if self.key:
-	#		CloseKey(self.key)
-	def __getitem__(self, index):
-		val, val_type = QueryValueEx(self.key, "ColorTable%02d" % index)
-		return val
-	def __setitem__(self, index, new_value):
-		SetValueEx(self.key, "ColorTable%02d" % index, 0, self.value_type, new_value)
-	### These should be replaced with slice comprehension in getitem and setitem:
-	def get_palette(self):
-		return [ self[x] for x in range(16) ]
-	def set_palette(self, new_palette):
-		self.old_values = self.get_palette()
-		for n, v in izip(count(0), new_palette):
-			self[n] = v
+	#	if self.regkey:
+	#		CloseKey(self.regkey)
+	def __getitem__(self, key):
+		if type(key)==slice:
+			iterator = iter(xrange(key.start or 0,
+								   self.colors if (key.stop == sys.maxint) else key.stop,
+								   key.step or 1))
+			return [ self[i] for i in iterator ]
+		else:
+			val, val_type = QueryValueEx(self.regkey, "ColorTable%02d" % key)
+			return val
+	def __setitem__(self, key, new_val):
+		if type(key)==slice:
+			iterator = iter(xrange(key.start or 0,
+								   self.colors if (key.stop == sys.maxint) else key.stop,
+								   key.step or 1))
+			for i in iterator:
+				self[i] = new_val[i]
+		else:
+			SetValueEx(self.regkey,				# name of key
+					   "ColorTable%02d" % key,	# name of subkey
+					   0,						# reserved
+					   self.value_type,			# colors are integers, see REG_DWORD above
+					   new_val
+					   )
 if __name__ == '__main__':
 	wcc = WindowsConsoleColor()
-	my_palette = [ 0x000000, 0x9e1828, 0xaece92, 0x968a38, 0x414171, 0x963c59, 0x418179, 0xbebebe, 0x666666, 0xcf6171, 0xc5f779, 0xfff796, 0x4186be, 0xcf9ebe, 0x71bebe, 0xffffff ]
-	my_palette = wcc.typical_colors
-	print "Current palette:", [ "0x%06x" % x for x in wcc.get_palette() ]
-	print "Setting palette to: ", [ "0x%06x" % x for x in my_palette ]
-	wcc.set_palette(my_palette)
+	apps = list(wcc.GetSettingsByApplication())
+	print "%d application(s) have custom settings: %r" %(len(apps), apps)
+	print "Current palette:", [ "0x%06x" % x for x in wcc[:] ]
+	if True:
+		new_palette = [ 0x000000,
+						0x004080,
+						0x40a8a8,
+						0x6ca8c8,
+						0xb9a060,
+						0x6000d2,
+						0xaf55a5,
+						0xc0c0c0,
+						0x808080,
+						0x8080ff,
+						0x00ff80,
+						0xc0ffff,
+						0xff6060,
+						0xff0060,
+						0xffffa8,
+						0xffffff ]
+		print "Setting palette to: ", [ "0x%06x" % x for x in new_palette ]
+		wcc[:] = new_palette
+		print "Now palette is:", [ "0x%06x" % x for x in wcc[:] ]
