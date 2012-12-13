dd7e5e3e3e87e2f10ea457b72480131941118196
Matt Hayden <hayden+github@morp.holo.gy>
2012-12-13 10:37:09 -0700

    tweaks

--- moon_phase.py
+++ moon_phase.py
@@ -3,6 +3,11 @@ from datetime import datetime
 import ephem
 from pytz import timezone, utc
 
+def is_visible(body):
+	ra = body.ra
+	if (0 <= ra <= 180):
+		return ra
+	return None
 # to see a list of cities, use ephem.cities._city_data.keys()
 try:
 	here = ephem.city('Boulder')
@@ -11,6 +16,7 @@ except:
 	here.name = 'Boulder'
 	#here.lat, here.lon, here.elevation = 40, -105.17, 1700
 	here.lat, here.lon, here.elevation = '40:1.28.30', '-105:15.35.09', 1700
+here.pressure = 0
 tz = timezone('US/Mountain')
 
 utcer = lambda d: utc.localize(d.datetime())
@@ -21,7 +27,10 @@ here.date = now
 
 sun = ephem.Sun(here)
 moon = ephem.Moon(here)
+venus = ephem.Venus(here)
 mars = ephem.Mars(here)
+jupiter = ephem.Jupiter(here)
+saturn = ephem.Saturn(here)
 
 ###
 events = []
@@ -31,6 +40,10 @@ def append(*args):
 # rising examples
 append("Sunrise", here.next_rising(sun))
 append("Moonrise", here.next_rising(moon))
+append("Venus rise", here.next_rising(venus))
+append("Mars rise", here.next_rising(mars))
+append("Jupiter rise", here.next_rising(jupiter))
+append("Saturn rise", here.next_rising(saturn))
 
 # setting examples 
 here.horizon='4' # add a custom value for Boulder
@@ -49,3 +62,11 @@ events.sort(key = lambda et: et[-1])
 for e, nt in events[:4]:
 	t = utcer(nt)
 	print e, localtimer(t), t-now
+if is_visible(moon):
+	print "Moon",
+if not is_visible(sun):
+	print "Venus" if is_visible(venus) else "",
+	print "Mars" if is_visible(mars) else "",
+	print "Jupiter" if is_visible(jupiter) else "",
+	print "Saturn" if is_visible(saturn) else "",
+print "visible"
