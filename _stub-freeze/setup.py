d35df51cb7a41a291929b22d5c76cb6f1d5d2621
Matt Hayden <hayden+git@morp.holo.gy>
2013-06-17 12:04:54 -0600

    Tweaks

--- _stub-freeze/setup.py
+++ _stub-freeze/setup.py
@@ -8,7 +8,7 @@ from cx_Freeze import setup, Executable
 #	threading
 #	tempfile
 ###
-net_excludes = 'email ftplib httplib ssl urllib xml xmllib xmlrpclib'.split()
+net_excludes = 'email ftplib httplib _ssl ssl urllib xml xmllib xmlrpclib'.split()
 dev_excludes = 'ctypes difflib distutils doctest inspect locale pdb unittest'.split()
 other_excludes = 'calendar tarfile zipfile'.split()
 
