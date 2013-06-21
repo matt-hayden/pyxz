a6b97723ce742ffb1a70e6a9aa3d365d5e5d58b1
Matt Hayden <hayden+git@morp.holo.gy>
2013-06-21 12:58:13 -0600

    Refactored

--- sanitize_strings.py
+++ sanitize_strings.py
@@ -1,4 +1,10 @@
+#!env python
 import os.path
+
+"""
+Remember that namedtuple has an option rename=True that numbers attributes if
+they have an illegal name.
+"""
 
 def path_sanitize(arg, sep='_', valid_characters='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-._'):
 	if os.path.isdir(arg):
@@ -9,7 +15,11 @@ def path_sanitize(arg, sep='_', valid_characters='0123456789abcdefghijklmnopqrst
 		return os.path.join(dirname, newname)
 	else:
 		return newname
-def sql_field_sanitize(arg, sep='', pass_brackets_through = True, valid_characters='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ#$%&/:@_`{|}~'):
+def sql_field_sanitize(arg,
+					   sep='',
+					   pass_brackets_through = True,
+					   valid_characters='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ#$%&/:@_`{|}~',
+					   pass_string_parts_through=lambda s:s.title()):
 	"""
 	Some programs, like SPSS, treat sep='', whereas some database exports treat
 	sep='_'.
@@ -24,7 +34,10 @@ def sql_field_sanitize(arg, sep='', pass_brackets_through = True, valid_characte
 		newname = ''.join(c if c in valid_characters else sep for c in name)
 	else:
 		d = ''.join(c if c in valid_characters else '_' for c in name)
-		newname = sep.join(_.title() for _ in d.split('_'))
+		if hasattr(pass_string_parts_through, '__call__'):
+			newname = sep.join(pass_string_parts_through(p) for p in d.split('_'))
+		else:
+			newname = d.replace('_','')
 	if bracketed and pass_brackets_through:
 		return '['+newname+']'
 	else:
