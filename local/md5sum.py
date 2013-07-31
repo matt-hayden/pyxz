f499f60e3fdf5f17f10773604e59b47500176964
Matt Hayden <hayden+git@morp.holo.gy>
2013-07-30 19:41:04 -0600

    Ready to test

--- local/md5sum.py
+++ local/md5sum.py
@@ -16,7 +16,7 @@ else:
 	md5sum_filename = 'MD5SUM'
 	md5sum_executable = 'md5sum'
 
-md5sum_file_cache = {}
+_md5sum_file_cache = {}
 def _gen_parsed_md5sum(filename):
 	debug('Reading '+filename)
 	with open(filename, 'Ur') as fi:
@@ -24,19 +24,29 @@ def _gen_parsed_md5sum(filename):
 			line = line.strip()
 			md5text, binary, filename = line[:32], line[33] == '*', line[34:]
 			yield (filename, binary, int(md5text, 16))
-def parse_md5sum_file(filename):
-	if filename in md5sum_file_cache:
-		return md5sum_file_cache[filename]
+def parse_md5sum_file(filename, reload=False):
+	global _md5sum_file_cache
+	if not reload and filename in _md5sum_file_cache:
+		return _md5sum_file_cache[filename]
 	else:
 		contents = list(_gen_parsed_md5sum(filename))
-		md5sum_file_cache[filename] = contents
+		_md5sum_file_cache[filename] = contents
 		return contents
+def _gen_md5sum_lookup():
+	global _md5sum_file_cache
+	for md5sum_filename, contents in _md5sum_file_cache.iteritems():
+		dirname, basename = os.path.split(md5sum_filename)
+		for filename, binary, md5 in contents:
+			yield os.path.join(dirname, filename), binary, md5
+def load_md5sum_cache():
+	return list(_gen_md5sum_lookup())
 #
-def md5summer(filenames,
-			  md5sum_filename=md5sum_filename,
-			  mode='a',
-			  skip=False,
-			  **kwargs):
+def md5maker(filenames,
+			 md5sum_filename=md5sum_filename,
+			 mode='a',
+			 skip=False,
+			 recurse=False,
+			 **kwargs):
 	"""
 	Generates MD5 checksums recursively. Checksum files are always generated
 	local to the input filenames, so multiple checksum files may be generated!
@@ -52,7 +62,9 @@ def md5summer(filenames,
 			error("Non-existent directory '{}' encountered, ignoring all {} files under it".format(root, len(files)))
 			continue
 		if dirs:
-			warning("directories {} in argument ignored".format(dirs))
+			if recurse:
+				for d in dirs: md5maker(os.listdir(d), recurse=recurse)
+			else: warning("non-recursive: directories {} ignored".format(dirs))
 		md5sum_file = os.path.join(root, md5sum_filename)
 		unanalyzed_files = set(files)
 		nf = len(unanalyzed_files)
@@ -67,3 +79,17 @@ def md5summer(filenames,
 			debug("Running {} {}{}{}".format(md5sum_executable, root, os.path.sep, list(unanalyzed_files)))
 			with open(md5sum_file, mode) as refile:
 				subprocess.check_call([md5sum_executable]+list(unanalyzed_files), stdout=refile, cwd=root)
+			parse_md5sum_file(md5sum_file, reload=True) # refresh the cache
+def md5reader(filenames):
+	"""
+	In this function, paths are local. This may complicate lists moved from
+	different systems.
+	"""
+	filenames = set(filenames)
+	cache = {path:(binary,md5) for path, binary, md5 in load_md5sum_cache()}
+	unanalyzed_files = filenames - set(cache)
+	if unanalyzed_files:
+		md5maker(unanalyzed_files) # should refresh the cache
+#		del cache
+	cache = {path:(binary,md5) for path, binary, md5 in load_md5sum_cache()}
+	return [(p, cache[p]) for p in filenames]
--- sizemo.py
+++ sizemo.py
@@ -1,4 +1,10 @@
 #!env python
+"""Find likely-duplicate files as fast as Python is able.
+
+A listing of files and their sizes is kept in tab-separated format. This format
+can be generated without Python:
+	find -type f -printf '%P\\t%s\\n' > .sizes
+"""
 from collections import Counter, defaultdict
 from logging import debug, info, warning, error, critical
 import os
@@ -7,36 +13,22 @@ import re
 import subprocess
 import sys
 
-from logging import debug, info, warning, error, critical
-import os.path
-import sys
-
 from local.flatten import flatten
 #from local.xglob import glob
-from local.md5sum import md5summer
+import local.md5sum as md5sum
+from local.units import human_readable_bytes
 from local.walk import *
-
 from local.xmimetypes import types_map
 
 from msys_path import *
 
-"""
-
-"""
-ignore_types = [ 'application/x-javascript', 'application/x-sh', 'application/x-python-code',
-				 'text/plain', 'text/html', 'text/xml', 'text/css', 'text/x-python',
-				 '.log' ]
-"""
-A listing of files and their sizes is kept in tab-separated format. This format
-can be generated without Python:
-	find -type f -printf '%P\t%s\n' > .sizes
-"""
+path_filter=(os.path.normpath if sys.platform.startswith('win') else None)
 size_db_filename = '.sizes'
 
-def rebase_sizes_files(root, path_filter=(os.path.normpath if sys.platform.startswith('win') else lambda _:_)):
+def rebase_sizes_files(root, path_filter=path_filter):
 	"""
 	GENERATOR
-	Re-create a list of <path>\t<bytes>\n for '.sizes' files 
+	Re-create a list of <path>\\t<bytes>\\n for '.sizes' files found under root. 
 	
 	Some platform path issues can be avoided by manipulating paths:
 	path_filter=os.path.normpath on Windows, for instance.
@@ -51,10 +43,13 @@ def rebase_sizes_files(root, path_filter=(os.path.normpath if sys.platform.start
 					fn, st = line.rstrip().split('\t')
 					s = int(st)
 					total_size += s
-					yield os.path.join(reporoot, path_filter(fn)), s
+					if path_filter:
+						yield os.path.join(reporoot, path_filter(fn)), s
+					else:
+						yield os.path.join(reporoot, fn), s
 				except Exception as e:
 					critical("Parsing error in {}:{}".format(fi.name, e))
-		info("{} total bytes represented in {}".format(total_size, reporoot))
+		info("Total {1} = {0} B represented in {}".format(total_size, " ".join(human_readable_bytes(total_size)), reporoot))
 def get_files_by_type_and_size(roots,
 							   ignore_types=[],
 							   check_if_exist=(sys.platform.startswith('win'))):
@@ -93,36 +88,61 @@ def get_files_by_type_and_size(roots,
 def get_size_by_type(roots=[],
 					 files_by_type_and_size=[],
 					 key=None):
+	"""
+	Print a table of the space used by different data types.
+	"""
 	assert roots or files_by_type_and_size
 	files_by_type_and_size = files_by_type_and_size or get_files_by_type_and_size(roots, ignore_types=ignore_types)
 	if not key:
 		def key(mimetype):
-			if mimetype.startswith('application'):
-				return mimetype
+			if mimetype.startswith('application'): return mimetype
 			else:
-				try:
-					return mimetype.split('/', 1)[0]
-				except:
-					return mimetype
+				try:	return mimetype.split('/', 1)[0]
+				except:	return mimetype
 	print
-	total_size = sum(len(fl)*s for (t, s), fl in files_by_type_and_size.iteritems())
+	total_size = sum(len(fs)*s for (t, s), fs in files_by_type_and_size.iteritems())
 	size_by_type = Counter()
-	for (t, s), fl in files_by_type_and_size.iteritems():
-		size_by_type[key(t)] += s*len(fl)
+	for (t, s), fs in files_by_type_and_size.iteritems():
+		size_by_type[key(t)] += s*len(fs)
 	total_size = float(sum(size_by_type.values()))
 	v = sorted(size_by_type.items(), key=lambda _:_[-1], reverse=True)
 	width = max(len(str(_[0])) for _ in v)+1
-	print "{:<{width}}{:^13}{:^5}".format("Type", "MB", "%", width=width)
+	print "{:<{width}}{:^13}{:^5}".format("Type", "Size", "%", width=width)
 	for t, s in v:
-		print "{:_<{width}}{:_>13.0f}{:_>5.0%}".format(t, s/1E6, s/total_size, width=width)
-	print "{:<{width}}{:>13.0f}".format("Total", total_size/1E6, width=width)
+		print "{:_<{width}}{:_>13.0f}{:_>5.0%}".format(t, " ".join(human_readable_bytes(s)), s/total_size, width=width)
+	print "{:<{width}}{:>13.0f}".format("Total", " ".join(human_readable_bytes(total_size)), width=width)
 	return size_by_type
 def get_files_of_same_size(roots=[],
 						   files_by_type_and_size=[],
 						   minsize=0):
 	assert roots or files_by_type_and_size
 	files_by_type_and_size = files_by_type_and_size or get_files_by_type_and_size(roots, ignore_types=ignore_types)
-	files_of_same_size = [ fl for (t, s), fl in files_by_type_and_size.iteritems() if (len(fl) > 1) and (s >= minsize) ]
-	debug("Detected {:,} sets of the same size and type:".format(len(files_of_same_size)))
-	return files_of_same_size
+	if minsize:
+		return [ fs for (t, s), fs in files_by_type_and_size.iteritems() if (len(fs) > 1) and (s >= minsize) ]
+	else:
+		return [ fs for (t, s), fs in files_by_type_and_size.iteritems() if (len(fs) > 1) ]
 #
+def main(roots, ignore_types):
+	files_by_type_and_size = get_files_by_type_and_size(roots, ignore_types=ignore_types)
+	get_size_by_type(files_by_type_and_size=files_by_type_and_size)
+	files_of_same_size = get_files_of_same_size(files_by_type_and_size=files_by_type_and_size)
+	# UNIX might use os.samefile here
+	
+	# this could be useful to lookup md5s for files:
+	# md5_by_filename = dict(md5sum.md5reader(flatten(files_of_same_size)))
+	# but, we want to invert that lookup
+	lookup = defaultdict(list)
+	for path, md5pair in md5sum.md5reader(flatten(files_of_same_size)):
+		lookup[md5pair].append(path)
+	duplicates = [(fs, md5pair) for md5pair, fs in lookup.iteritems() if len(fs) > 1]
+	if duplicates:
+		print "The following files are probably duplicates:"
+		for (binary, md5), fs in duplicates:
+			print "{:1}{:X}".format("*" if binary else " ", md5), " ".join(fs)
+	return 0
+if __name__ == '__main__':	
+	ignore_types = [ 'application/x-javascript', 'application/x-sh', 'application/x-python-code',
+					 'text/plain', 'text/html', 'text/xml', 'text/css', 'text/x-python',
+					 '.log' ]
+	args = sys.argv[1:] or ['.']
+	sys.exit(main(args, ignore_types=ignore_types))
