7e26d83586528db181bcbc525ddf8827da7caf05
Matt Hayden <hayden+github@morp.holo.gy>
2013-10-30 08:50:09 -0600

    Initial config

---
+++ local/xini.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import ConfigParser
+
+from xcollections import Namespace
+
+class IniParser(ConfigParser.SafeConfigParser):
+	def contents(self):
+		for s in self.sections():
+			yield s, Namespace(self.items(section=s))
