078a77ebc8d1913dcfa35050a881441fd1bc43c9
Matt Hayden <hayden+github@morp.holo.gy>
2013-11-04 14:24:09 -0700

    Chapter code started

--- video-cli/FFMpegJSON.py
+++ video-cli/FFMpegJSON.py
@@ -19,7 +19,7 @@ def custom_split_command(video_filename,
 				info_filename='',
 				frames_filename='',
 				output_pattern='',
-				average_time_between_splits=300):
+				average_time_between_chapter_guesses=300):
 	"""
 	Timestamps are all decimal seconds.
 	"""
@@ -39,12 +39,49 @@ def custom_split_command(video_filename,
 	if not os.path.exists(info_filename):
 		raise NotImplementedError("No info JSON for {}".format(info_filename))
 	info("Using media file description from "+info_filename)
-	if not frames_filename:
-		basepath, ext = os.path.splitext(info_filename)
-		frames_filename = basepath+'.frames'+ext
-	if not frames_filename:
-		raise NotImplementedError("No frames JSON for {}".format(frames_filename))
-	info("Using frames from "+frames_filename)
+	#
+	with open(info_filename) as fi:
+		general = json.load(fi, parse_float=Decimal)
+	if video_filename.upper() != general['format']['filename'].upper():
+		info("Video filename from {} doesn't match {}".format(info_filename, video_filename))
+	#if size != int(general['format']['size']):
+	#	info("Size from {} doesn't match {}".format(info_filename, video_filename))
+	start_time = Decimal(general['format']['start_time'])
+	duration = Decimal(general['format']['duration'])
+	end_time = start_time+duration
+	
+	nchapters = len(general['chapters'])
+	info("{} chapter(s) specified in {}".format(nchapters or 'No', info_filename)
+	#
+	if nchapters:
+		raise NotImplementedError("No chapter code!")
+	else:
+		if not frames_filename:
+			basepath, ext = os.path.splitext(info_filename)
+			frames_filename = basepath+'.frames'+ext
+		if os.path.exists(frames_filename):
+			info("Using frames from {} to designate chapter marks".format(frames_filename))
+			with open(frames_filename) as fi:
+				frames_info = json.load(fi, parse_float=Decimal)['frames']
+			else:
+				frames_info = general['frames']
+			frames = sorted(frames_info, key=scene_key, reverse=True)
+			nchapters, rem = divmod(duration, average_time_between_chapter_guesses)
+			assert nchapters >= 1
+			if rem: nchapters += 1
+			selected_frames = frames[:int(nchapters)]
+			#
+			chapters = [ Decimal(f['pkt_pts_time']) for f in selected_frames ]
+		else:
+			raise NotImplementedError("No frames JSON for {}".format(frames_filename))
+	chapters.sort()
+	chapters.insert(0, start_time)
+	chapters.append(end_time)
+	#
+	"""
+	Output files will have the form input_filename_{}:{}.ext, where {}:{} are the start and end
+	in seconds.
+	"""
 	if not output_pattern:
 		dirname, basename = os.path.split(video_filename)
 		filepart, ext = os.path.splitext(basename)
@@ -54,33 +91,9 @@ def custom_split_command(video_filename,
 		output_pattern = filepart+'_{}'+ext
 	def output_namer(*args):
 		return output_pattern.format('_'.join(str(a) for a in args))
-	###
-	with open(info_filename) as fi:
-		general = json.load(fi, parse_float=Decimal)
-	if video_filename.upper() != general['format']['filename'].upper():
-		info("Video filename from {} doesn't match {}".format(info_filename, video_filename))
-	if size != int(general['format']['size']):
-		info("Size from {} doesn't match {}".format(info_filename, video_filename))
-	start_time = Decimal(general['format']['start_time'])
-	duration = Decimal(general['format']['duration'])
-	end_time = start_time+duration
-	###
-	if os.path.exists(frames_filename):
-		with open(frames_filename) as fi:
-			frames_info = json.load(fi, parse_float=Decimal)['frames']
-	else:
-		frames_info = general['frames']
-	frames = sorted(frames_info, key=scene_key, reverse=True)
-	nselected, rem = divmod(duration, average_time_between_splits)
-	assert nselected >= 1
-	if rem: nselected += 1
-	selected_frames = frames[:int(nselected)]
 	#
-	cuttimes = [ Decimal(f['pkt_pts_time']) for f in selected_frames ]
-	cuttimes.sort()
-	cuttimes.insert(0, start_time)
-	cuttimes.append(end_time)
-	cuts = zip(cuttimes, cuttimes[1:])
+	###
+	cuts = zip(chapters, chapters[1:])
 	output_names = [ output_namer(int(p[0]), int(p[-1])) for p in cuts ]
 	assert video_filename not in output_names
 	#
