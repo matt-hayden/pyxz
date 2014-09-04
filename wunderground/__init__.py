01ce3a74201af2e1c2306d15c301c578136ee367
Matt Hayden <matt@aquacraft.com>
2014-09-03 21:04:20 -0600

    Initial commit and TODOs

--- wunderground/__init__.py
+++ wunderground/__init__.py
@@ -6,7 +6,10 @@ import dateutil.parser
 
 from local.xcollections import Namespace
 
-key_string = '26ea4286e102b790'
+key_string = 'Your_Key' # enter your developer key here
+key_string = '26ea4286e102b790' # TODO
+
+# TODO: adjust these to local conditions
 conditions_url = 'http://api.wunderground.com/api/'+key_string+'/conditions/q/CO/Boulder.xml'
 forecast_url = 'http://api.wunderground.com/api/'+key_string+'/forecast/q/CO/Boulder.xml'
 
