43f01e6e647463cee8e609a2eefd888c6776985c
Matt Hayden <hayden+github@morp.holo.gy>
2016-09-28 18:10:09 -0600

    Before moving to Google Cloud

--- fetch.py
+++ fetch.py
@@ -1,9 +1,15 @@
 #! /usr/bin/env python
 
 #import logging
+import os, os.path
 import subprocess
-import urllib2
+import sys
+import urlparse
 
+try:
+	from shlex import quote
+except:
+	from pipes import quote
 
 #logger = logging.getLogger('' if '__main__' == __name__ else __name__)
 #debug, info, warning, error, fatal = logger.debug, logger.info, logger.warning, logger.error, logger.fatal
@@ -12,23 +18,57 @@ import urllib2
 def fetch(arg, scp_sudo=''):
 	""" Return the content of a URI, including scp://
 	"""
-	if isinstance(arg, (urllib2.urlparse.ParseResult, urllib2.urlparse.SplitResult)):
+	if isinstance(arg, (urlparse.ParseResult, urlparse.SplitResult)):
 		uri = arg
 	else:
-		uri = urllib2.urlparse.urlparse(arg)
+		uri = urlparse.urlparse(arg)
 	if uri.scheme in ('scp'):
-		path = uri.path[1:] if uri.path.startswith('/') else uri.path
+		path = quote(uri.path[1:] if uri.path.startswith('/') else uri.path)
 		command = [ 'ssh', uri.netloc ]
 		if scp_sudo:
 			command.append(scp_sudo)
 		command += [ 'cat', path ]
 	else:
 		command = [ 'curl', uri.netloc ]
-	proc = subprocess.Popen(command)
-	proc.communicate()
-	return proc.returncode == 0
-
+	return subprocess.call(command) == 0
+def substitute_urls(iterable):
+	items = list(iterable)
+	for i in range(len(items)-1, 0, -1):
+		if '--' == items[i]:
+			break
+		u = urlparse.urlparse(items[i])
+		if u.scheme:
+			items[i] = u
+		else:
+			break
+	return items
+def parse_args(argv=None):
+	if argv is None:
+		argv = sys.argv
+	mode = argv[1]
+	command = substitute_urls(argv[2:])
+	labels = [ urlparse.urlunparse(arg) for arg in command if \
+			   isinstance(arg, (urlparse.ParseResult, urlparse.SplitResult)) ]
+	if not labels:
+		return command
+	if mode.endswith('diff'):
+		pre_command = []
+		for t in labels:
+			pre_command.extend(['--label', t])
+		command = pre_command + command
+	elif mode.endswith('grep'):
+		command = [ '--label='+t for t in labels ]+command
+	else:
+		mode = 'fetch'
+	return [mode]+command
+		
 if '__main__' == __name__:
 	#logging.basicConfig(level=logging.DEBUG if __debug__ else logging.WARNING)
-	import sys
-	fetch(' '.join(sys.argv[1:]))
+	#fetch(' '.join(sys.argv[1:]))
+	command = []
+	for t in parse_args():
+		if isinstance(t, (urlparse.ParseResult, urlparse.SplitResult)):
+			command.extend(['<(', 'fetch', urlparse.urlunparse(t), ')'])
+		else:
+			command.append(t)
+	print ' '.join(command)
